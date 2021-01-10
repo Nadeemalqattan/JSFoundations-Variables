ff102bb70123923d763cdc671d9fa3c102cad085
hesham <hesham@heshams-iMac.local>
2021-01-10 12:41:17 +0300

    DONEgit add .

---
+++ story.js
@@ -0,0 +1,5 @@
+const name = "Nadeem";
+const age = 24;
+const hobby = "Diving";
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
